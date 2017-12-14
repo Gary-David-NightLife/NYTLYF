c8f2ec352434cdebacb67dbbf770c68f71fd7a87
DavidHanover <31142178+DavidHanover@users.noreply.github.com>
2017-12-14 09:47:01 -0800

    Lyft button is implemented but broken for now, errors all over the place

--- MyApplicationtwo/app/src/main/java/com/vacuity/myapplication/fragment/YelpFragment.java
+++ MyApplicationtwo/app/src/main/java/com/vacuity/myapplication/fragment/YelpFragment.java
@@ -16,12 +16,20 @@ import android.widget.Toast;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+import com.fasterxml.jackson.databind.deser.std.JsonLocationInstantiator;
 import com.vacuity.myapplication.R;
 import com.vacuity.myapplication.app.App;
 import com.vacuity.myapplication.models.Business;
+import com.vacuity.myapplication.models.Location;
 import com.vacuity.myapplication.models.model.YelpLab;
 import com.vacuity.myapplication.models.model.nytlyfLab;
 import com.vacuity.myapplication.volley.VolleySingleton;
+
+
+import com.lyft.lyftbutton.LyftButton;
+import com.lyft.lyftbutton.RideParams;
+import com.lyft.lyftbutton.RideTypeEnum;
+import com.lyft.networking.ApiConfig;
 
 /**
  * Created by Gary Straub on 11/13/2017.
@@ -29,6 +37,11 @@ import com.vacuity.myapplication.volley.VolleySingleton;
 
 public class YelpFragment extends Fragment{
     private Business mBusiness;
+    private Location mLocation;
+
+    GPSTracker trak = new GPSTracker(getActivity());
+    private android.location.Location mMyLocation = trak.getLocation();
+
     private TextView mTitle;
     private TextView Description;
     private NetworkImageView mImage;
@@ -54,6 +67,10 @@ public class YelpFragment extends Fragment{
         super.onCreate(savedInstanceState);
         String yelpID = (String) getArguments().getSerializable(ARG_YELP_ID);
         mBusiness = YelpLab.get(getActivity()).getBusiness(yelpID);
+        mLocation = mBusiness.getLocation();
+
+
+
     }
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
@@ -84,6 +101,25 @@ public class YelpFragment extends Fragment{
         Description = (TextView) v.findViewById(R.id.frag_desc);
         String d = Html.fromHtml(mBusiness.getDisplayPhone()).toString();
         Description.setText(d);
+        Description.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        Description = (TextView) v.findViewById(R.id.frag_address);
+        String z = mLocation.getAddress1();
+        Description.setText(z);
         Description.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -132,6 +168,22 @@ public class YelpFragment extends Fragment{
                 Toast.makeText(getContext(), "Button Like", Toast.LENGTH_SHORT).show();
             }
         });
+
+        ApiConfig apiConfig = new ApiConfig.Builder()
+                .setClientId("...")
+                .setClientToken("...")
+                .build();
+
+        LyftButton lyftButton = (LyftButton) findViewById(R.id.lyft_button);
+        lyftButton.setApiConfig(apiConfig);
+
+        RideParams.Builder rideParamsBuilder = new RideParams.Builder()
+                .setPickupLocation(37.7766048, -122.3943629)
+                .setDropoffLocation(37.759234, -122.4135125);
+        rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
+
+        lyftButton.setRideParams(rideParamsBuilder.build());
+        lyftButton.load();
 
 
 
