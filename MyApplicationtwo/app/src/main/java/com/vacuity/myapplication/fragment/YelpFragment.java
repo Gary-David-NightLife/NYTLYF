6c654d129c34cf10b89ca5be3b62d5c3906ab2c9
Gary Straub <gstraub@sfsu.edu>
2017-12-14 13:26:08 -0800

    Calendar works and rideshare buttons placed

--- MyApplicationtwo/app/src/main/java/com/vacuity/myapplication/fragment/YelpFragment.java
+++ MyApplicationtwo/app/src/main/java/com/vacuity/myapplication/fragment/YelpFragment.java
@@ -1,5 +1,8 @@
 package com.vacuity.myapplication.fragment;
 
+import android.graphics.Bitmap;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,17 +14,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageSwitcher;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.NetworkImageView;
+import com.lyft.lyftbutton.LyftButton;
+import com.lyft.lyftbutton.RideParams;
+import com.lyft.lyftbutton.RideTypeEnum;
+import com.lyft.networking.ApiConfig;
+import com.uber.sdk.android.core.UberSdk;
+import com.uber.sdk.android.rides.RideParameters;
+import com.uber.sdk.android.rides.RideRequestButton;
+import com.uber.sdk.core.auth.Scope;
+import com.uber.sdk.rides.client.ServerTokenSession;
+import com.uber.sdk.rides.client.SessionConfiguration;
 import com.vacuity.myapplication.R;
 import com.vacuity.myapplication.app.App;
+import com.vacuity.myapplication.connection.YelpFusionApi;
+import com.vacuity.myapplication.connection.YelpFusionApiFactory;
 import com.vacuity.myapplication.models.Business;
 import com.vacuity.myapplication.models.model.YelpLab;
 import com.vacuity.myapplication.models.model.nytlyfLab;
 import com.vacuity.myapplication.volley.VolleySingleton;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import retrofit2.Call;
+
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * Created by Gary Straub on 11/13/2017.
@@ -32,9 +56,21 @@ public class YelpFragment extends Fragment{
     private TextView mTitle;
     private TextView Description;
     private NetworkImageView mImage;
+    private NetworkImageView mImage1;
+    private NetworkImageView mImage2;
+    private NetworkImageView mImage3;
+
     private Button mButtonHere;
     private Button mButtonLike;
     private Button mButtonDislike;
+    ImageSwitcher mImageswitch;
+    ImageLoader imageLoader1;
+    ImageLoader imageLoader2;
+    ImageLoader imageLoader3;
+    LayoutInflater mLayoutInflater;
+    NetworkImageView m;
+    private static final String PICKUP_NICK = "CurrentLocation";
+    ArrayList<String> imgList;
 
 
 
@@ -101,8 +137,24 @@ public class YelpFragment extends Fragment{
             }
         });
         mImage = (NetworkImageView) v.findViewById(R.id.frag_img);
-        ImageLoader imageLoader = VolleySingleton.getInstance(App.getContext()).getImageLoader();
+        final ImageLoader imageLoader = VolleySingleton.getInstance(App.getContext()).getImageLoader();
         mImage.setImageUrl(mBusiness.getImageUrl(),imageLoader);
+        mImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mImage.setImageUrl(imgList.get(next()),imageLoader);
+            }
+        });
+
+
+
+        try{
+            SearchTask searchTask = new SearchTask();
+            searchTask.execute(mBusiness.getId());
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
 
         mButtonHere = (Button) v.findViewById(R.id.detail_here);
         mButtonHere.setOnClickListener(new View.OnClickListener() {
@@ -115,6 +167,7 @@ public class YelpFragment extends Fragment{
 
             }
         });
+
 
         mButtonDislike = (Button) v.findViewById(R.id.detail_dislike);
         mButtonDislike.setOnClickListener(new View.OnClickListener() {
@@ -133,9 +186,118 @@ public class YelpFragment extends Fragment{
             }
         });
 
+        ApiConfig apiConfig = new ApiConfig.Builder()
+                .setClientId("iXr4bNITC70T")
+                .setClientToken("UoIFugJ+AS0VT9pNdEi/f8H1elJtNAO0iNm7z7BWJtQf2XVKcTpyQ2zvxGNrnoEvQzpnU9ksbcfTTt+O4G18q5n0E0fKglhaZu0fAxmyv/SSYllHdfQhfbk=")
+                .build();
 
+        GPSTracker cL = new GPSTracker(getContext());
+        Location location = cL.getLocation();
+
+
+        LyftButton lyftButton = (LyftButton) v.findViewById(R.id.lyft_button);
+        lyftButton.setApiConfig(apiConfig);
+        RideParams.Builder rideParamsBuilder = new RideParams.Builder()
+                .setPickupLocation(location.getLatitude(), location.getLongitude())
+                .setDropoffAddress(mBusiness.getLocation().getAddress1());
+        rideParamsBuilder.setRideTypeEnum(RideTypeEnum.CLASSIC);
+        lyftButton.setRideParams(rideParamsBuilder.build());
+        lyftButton.load();
+
+        SessionConfiguration config = new SessionConfiguration.Builder()
+                // mandatory
+                .setClientId("vEX5G1MidLTFVz9k6TES3Wai006DfI2U")
+                // required for enhanced button features
+                .setServerToken("YSYqR2H2OK_ZQXMnfbG4kHy0n39kF6wk-QeKrfHg")
+                // required for implicit grant authentication
+                .setRedirectUri("<REDIRECT_URI>")
+                // required scope for Ride Request Widget features
+                .setScopes(Arrays.asList(Scope.RIDE_WIDGETS))
+                // optional: set sandbox as operating environment
+                .setEnvironment(SessionConfiguration.Environment.SANDBOX)
+                .build();
+        UberSdk.initialize(config);
+        ServerTokenSession session = new ServerTokenSession(config);
+
+        YelpLab tLab = YelpLab.get(getContext());
+        Business tmp = tLab.getBusiness().get(0);
+
+
+
+        RideParameters rideParametersCheapestProduct = new RideParameters.Builder()
+                .setPickupLocation(location.getLatitude(), location.getLongitude(), PICKUP_NICK, tmp.getLocation().getAddress1() )
+                .setDropoffLocation(mBusiness.getCoordinates().getLatitude(), mBusiness.getCoordinates().getLongitude(), mBusiness.getName(),
+                        mBusiness.getLocation().getAddress1())
+
+                .build();
+
+
+        RideRequestButton requestButton = (RideRequestButton) v.findViewById(R.id.uber_button);
+        requestButton.setRideParameters(rideParametersCheapestProduct);
+        requestButton.setSession(session);
+        requestButton.loadRideInformation();
+
+
+
+//        pager.addView(mImage2);
 
         return v;
+    }
+    private int curr= 0;
+
+    private int next(){
+        return (curr++)%imgList.size();
+    }
+
+
+
+
+    private class SearchTask extends AsyncTask<String, Integer, Business> {
+        private ArrayList<Business> mBusiness;
+        private Bitmap mBitmap;
+        private Location mLocation;
+        YelpFusionApiFactory yelpFusionApiFactory;
+        YelpFusionApi yelpFusionApi;
+        Business myResults;
+
+        public SearchTask()throws IOException {
+            yelpFusionApiFactory = new YelpFusionApiFactory();
+        }
+
+        @Override
+        protected Business doInBackground(String... strings) {
+            try{
+                yelpFusionApi = yelpFusionApiFactory.createAPI("7qCtCchb7Otc0_bGSIkucQ",
+                        "PFH4bBwEqBaiuRTkrVzqFvelfwI1gRqEHzxp5orCsl0ke0ORcN4lmOLpwPP9ept5");
+                myResults = buissnessSearchTest(strings[0]);
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+            return myResults;
+        }
+
+        private Business buissnessSearchTest(String key)throws IOException{
+            Call<Business> call = yelpFusionApi.getBusiness(key);
+            Business response = call.execute().body();
+            assertNotNull(response);
+            //Log.e("Response", String.valueOf(response.getBusinesses().size()));
+//            for(int i =0; i<response.getBusinesses().size(); i++){
+//                //Log.e("Response", String.valueOf(response.getBusinesses().get(i).getName()));
+//            }
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(Business businesses) {
+
+            imgList = new ArrayList<>();
+            for(int i =0; i<businesses.getPhotos().size(); i++ ){
+                imgList.add(businesses.getPhotos().get(i));
+            }
+
+
+            super.onPostExecute(businesses);
+        }
     }
 
 }
